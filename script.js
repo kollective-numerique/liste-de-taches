0806e65108d963d337db5a96174d807e664ee66d
seleshabani <seleshabani4@gmail.com>
2022-12-02 03:08:10 +0100

    debug (liés à la suppresion d'une tâche dans le localstorage et review de code) : suppression de l'item en storage en utilsant un id, editions des commentaires pour améliorer le contenu

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//les fonctions doivent avoir des noms qui parlent (vclose : il ne dit pas suffisament le rôle de la fonction)
 function vclose(){
 
     let li=document.getElementsByTagName("LI");
@@ -12,27 +13,34 @@ function vclose(){
     }
     let fermer = document.getElementsByClassName("close");
     for (let i = 0; i < fermer.length; i++) {
+      
+        // cette façon de faire appel aux events est actuellement de plus en plus dépracié 
+        // https://developer.mozilla.org/fr/docs/Learn/JavaScript/Building_blocks/Events (je te conseil de lire cet article)
         fermer[i].onclick = function (){
             let pa=this.parentElement;
+            let id = pa.dataset.id; //recupère le id de unique de l'elt (attribué lors de sa création)
             pa.style.display = "none";
-            // window.localStorage.removeItem(u);
-        }       
-        
-        
+            window.localStorage.removeItem(id);
+        }          
     }
 }
 let nTache=window.localStorage.getItem("nTache")
-if (nTache){
+
+/* if (nTache){
+
 }else{
     window.localStorage.setItem("nTache",0);
-    
-}
+} */
+if (!nTache) window.localStorage.setItem("nTache",0); // c'est plus court je trouve
 vclose();
+
 let listens=document.getElementById("uls");
 let k=0;
+
 for (let i=0; i< Number.parseInt(nTache); i++){
-     k=k+1;
+    k=k+1;
     let Liss=document.createElement("li");
+    Liss.setAttribute('data-id',k);
     let objts=window.localStorage.getItem(k);
     let objt=JSON.parse(objts);
     let tach=document.createTextNode('le '+objt.da+' :'+objt.ta)
@@ -42,29 +50,35 @@ for (let i=0; i< Number.parseInt(nTache); i++){
     vclose();
     
 }
-alert(objt);
+//alert(objt);
 
 
 var liste = document.querySelector('ul');
+
 liste.addEventListener('click', function(event) {
   if (event.target.tagName === 'LI') {
     event.target.classList.toggle('checked');
   }
 }, false);
+
+
 function newelement(){
+    let kache=Number.parseInt(window.localStorage.getItem("nTache"),10);
+    kache=kache+1;
     let Lis=document.createElement("li");
+    
+    Lis.setAttribute('data-id',kache); //atribue un identifiant unique à l'élèment crée dans le dom
+
     let tache=document.getElementById("tache").value;
     let date=document.getElementById('date').value;
     let taches=document.createTextNode('le '+date+' :'+tache)
     let listen=document.getElementById("uls");
   
+  // pour mettre en place un controle lors de la saisi le meilleur moyen est de passer par un event
   if (tache===''){
     alert('saisissez une tache');
   } else{
-  
-    
- 
-     
+     // pourquoi mettre un else si tu ne comptes rien mettre à l'intérieur
   }
   Lis.appendChild(taches);
   listen.appendChild(Lis);
@@ -72,10 +86,8 @@ function newelement(){
       da:date,
       ta:tache
  }
- let kache=Number.parseInt(window.localStorage.getItem("nTache"),10);
- kache=kache+1;
+ 
  window.localStorage.setItem(kache,JSON.stringify(talk));
  window.localStorage.setItem("nTache",kache);
-  
-  vclose(); 
-   }
+ vclose(); 
+}
